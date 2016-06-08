f73e18b210f30a9a5caac9c557e123a803fc5531
David Castro <SadFud@users.noreply.github.com>
2016-06-09 00:55:47 +0200

    Add files via upload

---
+++ wifi key recovery.bat
@@ -0,0 +1,26 @@
+@echo off
+:menu
+echo   "-------    ----   \          /  | /          "
+echo   "   |      |    |   \        /   |/           "
+echo   "   |      |    |    \  /\  /    |\           "
+echo   "___/       ----      \/  \/     | \  Recovery"
+echo (C) SadFud
+echo 1.Ver nueva configuracion
+echo 2.Salir
+set/p opc=Seleccionar opcion=
+if %opc%==1 goto ver
+if %opc%==2 goto salir
+:ver
+cls
+netsh wlan show profile
+pause>nul
+set/p perfil=Introduce el nombre del perfil wifi=
+cls
+echo La calave se encontrara en la linea "Contenido de la clave."
+netsh wlan show profile %perfil% key=clear
+pause>nul
+goto menu
+:salir
+exit
+
+
